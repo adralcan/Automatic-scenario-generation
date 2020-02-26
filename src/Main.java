b133d1275a4576037b2642ee7ed32af59a9dd4c8
adralcan <adrian97ad@gmail.com>
2020-02-26 09:14:48 +0100

    Trabajando en fundamentos de J

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Vamos a leer información de la consola.
-        String usuario = scanner.nextLine();
+        System.out.println("Proporciona el titulo: ");
+        String titulo = scanner.nextLine();
+        System.out.println("Proporciona el autor: ");
+        String autor = scanner.nextLine();
+        System.out.println(titulo + " fue escrito por " + autor);
     }
 }
