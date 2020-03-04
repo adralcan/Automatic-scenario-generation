2dff4ab2b66bb6a0e896941079eeb926b2487f4f
adralcan <adrian97ad@gmail.com>
2020-03-04 19:23:04 +0100

    Trabajando en la clase Grafo y en el curso de Java

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,18 @@
+import java.util.ArrayList;
+
 public class Graph {
+    // Variables de instancia
+    private int _V; // numero de vertices
+    private int _E; // numero de aristas
+    private ArrayList<ArrayList<Element>> _ady; // vector de listas de adyacentes
+
+    Graph(int v) {
+        _V = v;
+        _E = 0;
+        _ady = new ArrayList<>(_V);
+    }
+
+    int V() { return _V;}
+    int E() { return _E;}
 
 }
