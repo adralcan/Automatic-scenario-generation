5b754e5fc5f6e6e5a82a135887b60ff60cf331ea
adralcan <adrian97ad@gmail.com>
2020-03-11 18:33:34 +0100

    First Graph implementation

--- src/Graph.java
+++ src/Graph.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.lang.IllegalArgumentException;
 
 public class Graph {
     // Variables de instancia
+    // Si los atributos no llevan private se asumme que son públicos
     private int _V; // numero de vertices
     private int _E; // numero de aristas
     private ArrayList<ArrayList<Element>> _ady; // vector de listas de adyacentes
@@ -12,7 +14,23 @@ public class Graph {
         _ady = new ArrayList<>(_V);
     }
 
-    int V() { return _V;}
-    int E() { return _E;}
+    public int V() { return _V;}
+    public int E() { return _E;}
 
+
+    public void putEdge(int v, int w) {
+        if (v >= _V || w >= _V) {
+            throw new IllegalArgumentException();
+        }
+        ++_E;
+        _ady.get(v).add(new Element());
+        _ady.get(w).add(new Element());
+    }
+
+    public ArrayList<Element> ady(int v) {
+        if (v >= _V) {
+            throw new IllegalArgumentException();
+        }
+        return _ady.get(v);
+    }
 }
---
+++ src/Vertex.java
@@ -0,0 +1,6 @@
+public class Vertex {
+    String label;
+    Vertex(String label) {
+        this.label = label;
+    }
+}
