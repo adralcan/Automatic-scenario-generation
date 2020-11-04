18879bdae6299398c65d567a2787b6326abb503c
adralcan <adrian97ad@gmail.com>
2020-11-04 18:43:38 +0100

    Block creada, reorganizar MapObject para hacer subclases según el tipo de objeto

--- src/map/Block.java
+++ src/map/Block.java
@@ -1,4 +1,49 @@
 package map;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Block {
+    private String name;
+    private String category;
+    private int max;
+
+    private final List<String> actionCategory = Arrays.asList("advance", "backwards", "turnLeft, turnRight", "jump", "action");
+    private final List<String> controlCategory = Arrays.asList("loop", "if", "ifElse", "whileLoop", "infiniteLoop");
+    private final List<String> operatorCategory = Arrays.asList("bridgeCondition", "tilePositionCondition", "not", "logic_operation");
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;*
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    private Boolean findBlockInList(String name, List<String> category) {
+        for (String element : category) {
+            if (element.contains(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Block(String name, int max) {
+        if (findBlockInList(name, actionCategory)) {
+            category = "action";
+        } else if (findBlockInList(name, controlCategory)) {
+            category = "control";
+        } else if (findBlockInList(name, operatorCategory)) {
+            category = "operator";
+        } else {
+            throw new IllegalArgumentException("This block name does not exists: " + name);
+        }
+        this.name = name;
+        this.max = max < 1 ? 0 : max;
+    }
 }
--- src/map/MapTemplate.java
+++ src/map/MapTemplate.java
@@ -1,16 +1,26 @@
 package map;
 
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapTemplate {
     private String type;
     private int width;
     private int height;
-    private Vector<Block> blocks;
-    private Vector<Tile> tiles;
-    private Vector<MapObject> objects;
-    private Vector<Player> player;
-    private Vector<String> solution;
+    private List<Block> blocks;
+    private List<Tile> tiles;
+    private List<MapObject> objects;
+    private List<Player> player;
+    private List<String> solution;
+
+    // Other attrs from json file that content map info
+    // ...
+
+    public MapTemplate(String type, int w, int h, String solution){
+        width = w;
+        height = h;
+        tiles = new ArrayList<>(width*height);
+    }
 
     // Other attrs from json file that content map info
 }
